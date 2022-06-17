2eacc3395736cb5e5530ffd8e4c8200d6802a007
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-06-18 01:46:46 +0700

    Remove redundant radio buttons

--- frontend/src/views/v-authorship.vue
+++ frontend/src/views/v-authorship.vue
@@ -49,12 +49,6 @@
             v-on:click="indicateSearchBar(); updateSearchBarValue();"
           ) Filter
       .fileTypes
-        input.radio-button--checkbox(
-          type="radio",
-          value="checkboxes",
-          v-model="filterType",
-          v-on:change="indicateCheckBoxes"
-        )
         .checkboxes.mui-form--inline(v-if="files.length > 0")
           label(style='background-color: #000000; color: #ffffff')
             input.mui-checkbox--fileType#all(type="checkbox", v-model="isSelectAllChecked")
